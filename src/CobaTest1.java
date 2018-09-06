02019bff859c5cdbc25627110913bbada1144f54
ajipandi <ajipandi14@yahoo.com>
2018-09-06 21:36:24 +0700

    Coba 1

---
+++ src/CobaTest1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nuser
+ */
+public class CobaTest1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah=5;
+        for(int i=1 ;i<=jumlah;i++){
+        for (int j=1;j<=i;j++){
+            System.out.print("A");
+        }
+            System.out.println("");
+        }
+    }
+    
+}
